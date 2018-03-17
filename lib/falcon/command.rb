871d6cda049878c4781ee13994ab53705f273041
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2018-03-18 11:31:53 +1300

    Prepare for more commands.

--- lib/falcon/command.rb
+++ lib/falcon/command.rb
@@ -18,93 +18,14 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-require_relative 'server'
-require_relative 'verbose'
-
-require 'async/container'
-require 'async/io/trap'
+require_relative 'command/serve'
 
 require 'samovar'
-require 'etc'
-
-require 'rack/builder'
-require 'rack/server'
 
 module Falcon
 	module Command
-		def self.default_concurrency
-			Etc.nprocessors
-		rescue
-			2
-		end
-		
 		def self.parse(*args)
 			Top.parse(*args)
-		end
-		
-		class Serve < Samovar::Command
-			self.description = "Run an HTTP server."
-			
-			options do
-				option '-c/--config <path>', "Rackup configuration file to load", default: 'config.ru'
-				option '-n/--concurrency <count>', "Number of processes to start", default: Command.default_concurrency, type: Integer
-				
-				option '-b/--bind <address>', "Bind to the given hostname/address", default: "tcp://localhost:9292"
-				
-				option '--forked | --threaded', "Select a specific concurrency model", key: :container, default: :threaded
-			end
-			
-			def container_class
-				case @options[:container]
-				when :threaded
-					require 'async/container/threaded'
-					return Async::Container::Threaded
-				when :forked
-					require 'async/container/forked'
-					return Async::Container::Forked
-				end
-			end
-			
-			def load_app(verbose)
-				app, options = Rack::Builder.parse_file(@options[:config])
-				
-				if verbose
-					app = Verbose.new(app)
-				end
-				
-				return app, options
-			end
-			
-			def run(verbose)
-				app, options = load_app(verbose)
-				
-				Async.logger.info "Falcon taking flight! Binding to #{@options[:bind]} [#{container_class} with concurrency: #{@options[:concurrency]}]"
-				
-				debug_trap = Async::IO::Trap.new(:USR1)
-				
-				container_class.new(concurrency: @options[:concurrency]) do |task|
-					task.async do
-						debug_trap.install!
-						Async.logger.info "Send `kill -USR1 #{Process.pid}` for detailed status :)"
-						
-						debug_trap.trap do
-							task.reactor.print_hierarchy($stderr)
-						end
-					end
-					
-					server = Falcon::Server.new(app, [
-						Async::IO::Endpoint.parse(@options[:bind], reuse_port: true)
-					])
-					
-					server.run
-				end
-			end
-			
-			def invoke(parent)
-				container = run(parent.verbose?)
-				
-				sleep
-			end
 		end
 		
 		class Top < Samovar::Command
