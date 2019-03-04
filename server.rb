c975c6d8e4be25b250806546e888e9ff8a788a87
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2019-03-04 17:29:24 +1300

    Add support for all 3 sites.

--- server.rb
+++ server.rb
@@ -1,5 +1,13 @@
 #!/usr/bin/env -S ./bin/falcon virtual --bind-insecure http://[::]:1080 --bind-secure https://[::]:1443
 
-rack 'localhost', :self_signed do
+rack 'benchmark.localhost', :self_signed do
 	root File.expand_path("examples/benchmark/", __dir__)
+end
+
+rack 'beer.localhost', :self_signed do
+	root File.expand_path("examples/beer/", __dir__)
+end
+
+rack 'hello.localhost', :self_signed do
+	root File.expand_path("examples/hello/", __dir__)
 end
