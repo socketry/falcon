2e6c5c8b0a5322113c546dff1e894702ddf336d5
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2019-06-11 22:39:43 +1200

    Fix alpn negotiation.

--- lib/falcon/hosts.rb
+++ lib/falcon/hosts.rb
@@ -62,7 +62,18 @@ module Falcon
 				end
 				
 				context.session_id_context = "falcon"
-				context.alpn_protocols = DEFAULT_ALPN_PROTOCOLS
+				
+				context.alpn_select_cb = lambda do |protocols|
+					if protocols.include? "h2"
+						return "h2"
+					elsif protocols.include? "http/1.1"
+						return "http/1.1"
+					elsif protocols.include? "http/1.0"
+						return "http/1.0"
+					else
+						return nil
+					end
+				end
 				
 				context.set_params(
 					ciphers: SERVER_CIPHERS,
