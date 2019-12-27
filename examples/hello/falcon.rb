2986c33b6e286757735ec92f4e29bacaf1e886be
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2019-12-27 21:24:58 +1300

    Playing around with `falcon host` binding to local port.

--- examples/hello/falcon.rb
+++ examples/hello/falcon.rb
@@ -2,7 +2,14 @@
 
 load :rack, :self_signed_tls, :supervisor
 
-rack 'hello.localhost', :self_signed_tls
+rack 'hello.localhost', :self_signed_tls do
+	endpoint do
+		Async::HTTP::Endpoint.parse("http://localhost:9292")
+	end
+	
+	protocol {Async::HTTP::Protocol::HTTP1}
+	scheme 'http'
+end
 
 # supervisor
 
