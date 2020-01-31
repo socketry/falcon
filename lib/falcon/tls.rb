42989c902416be4df6383a707e99f023d39948a5
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2020-01-31 23:20:24 +1300

    Use recommended TLS 1.2 and 1.3 ciphers.

--- lib/falcon/tls.rb
+++ lib/falcon/tls.rb
@@ -22,17 +22,23 @@ require_relative 'extensions/openssl'
 
 module Falcon
 	module TLS
+		# We follow "Intermediate compatibility"
+		# https://wiki.mozilla.org/Security/Server_Side_TLS
 		SERVER_CIPHERS = [
-			"HIGH",
-			"!aNULL",
-			"!eNULL",
-			"@STRENGTH"
-			# "ECDHE-RSA-AES256-GCM-SHA512",
-			# "DHE-RSA-AES256-GCM-SHA512",
-			# "ECDHE-RSA-AES256-GCM-SHA384",
-			# "DHE-RSA-AES256-GCM-SHA384",
-			# "ECDHE-RSA-AES256-SHA384",
-			# "ECDHE-RSA-AES128-GCM-SHA256",
+			# TLS 1.3:
+			"TLS_AES_128_GCM_SHA256",
+			"TLS_AES_256_GCM_SHA384",
+			"TLS_CHACHA20_POLY1305_SHA256",
+			
+			# TLS 1.2:
+			"ECDHE-ECDSA-AES128-GCM-SHA256",
+			"ECDHE-RSA-AES128-GCM-SHA256",
+			"ECDHE-ECDSA-AES256-GCM-SHA384",
+			"ECDHE-RSA-AES256-GCM-SHA384",
+			"ECDHE-ECDSA-CHACHA20-POLY1305",
+			"ECDHE-RSA-CHACHA20-POLY1305",
+			"DHE-RSA-AES128-GCM-SHA256",
+			"DHE-RSA-AES256-GCM-SHA384"
 		].freeze
 	end
 end
