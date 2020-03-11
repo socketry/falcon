5ef00024120b93499cc747ef1a7612a4fe528e88
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2020-03-12 12:20:31 +1300

    Improve spec performance.

--- spec/rack/handler/shared_examples.rb
+++ spec/rack/handler/shared_examples.rb
@@ -38,10 +38,20 @@ RSpec.shared_examples_for Rack::Handler do |server_name|
 			Async::Process.spawn("rackup", "--server", server_name, "--host", endpoint.hostname, "--port", endpoint.port.to_s, config_path)
 		end
 		
-		Async::Task.current.sleep 2
+		response = nil
 		
-		response = client.post("/", {}, ["Hello World"])
-	
+		# This mess is because there is no way to know when the above process is ready to handle connections...
+		10.times do |i|
+			begin
+				response = client.post("/", {}, ["Hello World"])
+			rescue Errno::ECONNREFUSED
+				Async::Task.current.sleep(i/10.0)
+				retry
+			else
+				break
+			end
+		end
+		
 		expect(response).to be_success
 		expect(response.read).to be == "Hello World"
 		
