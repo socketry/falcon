d9af28b252eb5311df1125d5b416ff99281eb952
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2020-03-24 20:51:47 +1300

    Add standalone websocket server example.

---
+++ examples/server/standalone.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby
+
+require 'async'
+require 'async/http/endpoint'
+require 'async/websocket/adapters/rack'
+
+require 'falcon'
+
+module WebSocketApp
+	def self.call(env)
+		Async::WebSocket::Adapters::Rack.open(env, protocols: %w[ws]) do |connection|
+			while (message = connection.read)
+				pp message
+			end
+		end or [200, [], ["Websocket only."]]
+	end
+end
+
+Async do
+	websocket_endpoint = Async::HTTP::Endpoint.parse('http://127.0.0.1:3000')
+	
+	app = Falcon::Server.middleware(WebSocketApp)
+	
+	server = Falcon::Server.new(app, websocket_endpoint)
+	
+	server.run.each(&:wait)
+end
