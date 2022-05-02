01a47108d8dc79401478bf9e6496a85cbed24a85
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2022-05-03 00:16:07 +1200

    Update server context test context.

--- spec/falcon/server_context.rb
+++ spec/falcon/server_context.rb
@@ -1,43 +1,66 @@
-# frozen_string_literal: true
+# Copyright, 2019, by Samuel G. D. Williams. <http://www.codeotaku.com>
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+require 'async/http/server'
+require 'async/http/client'
+require 'async/http/endpoint'
+require 'async/io/shared_endpoint'
 
 require 'falcon/server'
-require 'async/http/client'
-require 'async/rspec/reactor'
-
-require "async/http/client"
-require "async/http/endpoint"
 
 RSpec.shared_context Falcon::Server do
 	include_context Async::RSpec::Reactor
 	
 	let(:protocol) {Async::HTTP::Protocol::HTTP1}
-	let(:endpoint) {Async::HTTP::Endpoint.parse('http://127.0.0.1:9294', reuse_port: true)}
-	let!(:client) {Async::HTTP::Client.new(endpoint, protocol: protocol)}
+	let(:endpoint) {Async::HTTP::Endpoint.parse('http://127.0.0.1:9294', timeout: 0.8, reuse_port: true, protocol: protocol)}
 	
-	let!(:server_task) do
-		server
-		
-		reactor.async do
-			server.run
-		end
-	end
-	
-	after(:each) do
-		server_task.stop
-		client.close
-	end
-	
-	let(:app) do
-		lambda do |env|
-			[200, {}, []]
-		end
-	end
+	let(:retries) {1}
 	
 	let(:middleware) do
 		Falcon::Server.middleware(app)
 	end
 	
 	let(:server) do
-		Falcon::Server.new(middleware, endpoint, protocol: protocol)
+		Falcon::Server.new(middleware, @bound_endpoint)
 	end
+	
+	before do
+		@client = Async::HTTP::Client.new(endpoint, protocol: endpoint.protocol, retries: retries)
+		
+		# We bind the endpoint before running the server so that we know incoming connections will be accepted:
+		@bound_endpoint = Async::IO::SharedEndpoint.bound(endpoint)
+		
+		# I feel a dedicated class might be better than this hack:
+		allow(@bound_endpoint).to receive(:protocol).and_return(endpoint.protocol)
+		allow(@bound_endpoint).to receive(:scheme).and_return(endpoint.scheme)
+		
+		@server_task = Async do
+			server.run
+		end
+	end
+	
+	after do
+		@client.close
+		@server_task.stop
+		@bound_endpoint.close
+	end
+	
+	let(:client) {@client}
 end
