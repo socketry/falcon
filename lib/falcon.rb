112b7a7b262f430a2ad06b5485ff5398f9d16098
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2024-01-31 17:27:07 +1300

    Ensure `isolation_level` is set. (#219)

--- lib/falcon.rb
+++ lib/falcon.rb
@@ -4,3 +4,6 @@
 # Copyright, 2017-2023, by Samuel Williams.
 
 require_relative "falcon/server"
+
+# Falcon, running on Rails, requires specific configuration:
+require_relative "falcon/railtie" if defined?(Rails::Railtie)
