af5374df0bd294944076db3b6fb1994353b5bdf0
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2024-01-31 17:44:45 +1300

    Update copyrights.

--- lib/falcon/railtie.rb
+++ lib/falcon/railtie.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+# Released under the MIT License.
+# Copyright, 2024, by Samuel Williams.
+
 module Falcon
 	class Railtie < Rails::Railtie
 		config.active_support.isolation_level = :fiber
