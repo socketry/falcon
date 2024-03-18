1a7d220f09508de96e256027ba7323fc5a562194
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2024-03-18 20:43:38 +1300

    Less noisy.

--- config/sus.rb
+++ config/sus.rb
@@ -7,4 +7,4 @@ require 'covered/sus'
 include Covered::Sus
 
 require 'openssl'
-$stderr.puts "OpenSSL::OPENSSL_LIBRARY_VERSION: #{OpenSSL::OPENSSL_LIBRARY_VERSION}"
+# $stderr.puts "OpenSSL::OPENSSL_LIBRARY_VERSION: #{OpenSSL::OPENSSL_LIBRARY_VERSION}"
