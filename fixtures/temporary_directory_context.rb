6854c23ffa23ea1b7e06ab1b744060dd725c5e2a
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2024-08-25 14:05:05 +1200

    Extract temporary directory setup into fixture.

---
+++ fixtures/temporary_directory_context.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+# Released under the MIT License.
+# Copyright, 2024, by Samuel Williams.
+
+require 'tmpdir'
+
+module TemporaryDirectoryContext
+	def around(&block)
+		Dir.mktmpdir do |root|
+			@root = root
+			super(&block)
+			@root = nil
+		end
+	end
+	
+	attr :root
+end
