19fe8ece7cc49aa03222afe2c940682aeb69fe37
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2024-09-05 19:49:45 +1200

    Remove gems.locked from websockets example to unpin version.

--- examples/websockets/gems.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-# Released under the MIT License.
-# Copyright, 2022-2023, by Samuel Williams.
-
-source "https://rubygems.org"
-
-gem "falcon", path: "../../"
-gem "async-websocket"
