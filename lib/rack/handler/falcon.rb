dc54ccdcba8946b153685961b90adab4c34c5e04
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2025-01-05 11:48:36 +1300

    Improve compatibility of rackup handler, w.r.t sinatra.

--- lib/rack/handler/falcon.rb
+++ lib/rack/handler/falcon.rb
@@ -8,9 +8,15 @@ require "rack/handler"
 
 require_relative "../../falcon/rackup/handler"
 
+# Generally speaking, you should not require this file directly, or assume the existance of the `Rack::Handler::Falcon` constant. Instead, use `Rack::Handler.get(:falcon)` to load and access the handler.
+
 module Rack
 	module Handler
-		Falcon = ::Falcon::Rackup::Handler
-		register :falcon, Falcon
+		# Rack (v2) expects the constant to be in the `Rack::Handler` namespace, so we define a new handler class in the `Rack::Handler` namespace that inherits from `Falcon::Rackup::Handler`.
+		class Falcon < ::Falcon::Rackup::Handler
+		end
+		
+		# Rack (v2) expects a string for the handler constant name. `Falcon.to_s` returns a more human friendly name, so we explicitly pass `Falcon.name` to `register` to ensure Rack can find the handler using the registered name.
+		register :falcon, Falcon.name
 	end
 end
