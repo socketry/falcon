226ff1acc0b581402546181a31683f3941bf2893
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2025-01-29 19:23:41 +1300

    Remove validate middleware - no longer required - changes made in async-http gem.

--- lib/falcon/server.rb
+++ lib/falcon/server.rb
@@ -10,7 +10,6 @@ require "protocol/http/content_encoding"
 
 require "async/http/cache"
 require_relative "middleware/verbose"
-require_relative "middleware/validate"
 require "protocol/rack"
 
 module Falcon
@@ -18,17 +17,12 @@ module Falcon
 	class Server < Async::HTTP::Server
 		# Wrap a rack application into a middleware suitable the server.
 		# @parameter rack_app [Proc | Object] A rack application/middleware.
-		# @parameter validate [Boolean] Whether to add the {Middleware::Validate} middleware.
 		# @parameter verbose [Boolean] Whether to add the {Middleware::Verbose} middleware.
 		# @parameter cache [Boolean] Whether to add the {Async::HTTP::Cache} middleware.
-		def self.middleware(rack_app, verbose: false, validate: true, cache: true)
+		def self.middleware(rack_app, verbose: false, cache: true)
 			::Protocol::HTTP::Middleware.build do
 				if verbose
 					use Middleware::Verbose
-				end
-				
-				if validate
-					use Middleware::Validate
 				end
 				
 				if cache
