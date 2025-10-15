35444d56dc651a695ada8fc82730a6f11fa8116b
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2025-10-15 17:56:37 +1300

    Update `after_gem_release_version_increment`.

--- bake.rb
+++ bake.rb
@@ -8,5 +8,5 @@
 # @parameter version [String] The new version number.
 def after_gem_release_version_increment(version)
 	context["releases:update"].call(version)
-	context["utopia:project:readme:update"].call
+	context["utopia:project:update"].call
 end
