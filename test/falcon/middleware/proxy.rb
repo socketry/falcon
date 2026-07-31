b80e738824fba229b7d19894669aa86ad20fc278
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2026-07-31 21:51:42 +1200

    Modernize code.

--- test/falcon/middleware/proxy.rb
+++ test/falcon/middleware/proxy.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # Released under the MIT License.
-# Copyright, 2018-2024, by Samuel Williams.
+# Copyright, 2018-2026, by Samuel Williams.
 # Copyright, 2026, by Fletcher Dares.
 
 require "falcon/middleware/proxy"
--- test/falcon/middleware/redirect.rb
+++ test/falcon/middleware/redirect.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # Released under the MIT License.
-# Copyright, 2026, by Fletcher Dares.
+# Copyright, 2026, by Samuel Williams.
 
 require "falcon/middleware/redirect"
 
