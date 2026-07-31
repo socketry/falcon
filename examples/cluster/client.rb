8c99f6871043a0f8eef3386c67cf4a3d9aecd490
Samuel Williams <samuel.williams@shopify.com>
2026-07-31 21:07:08 +1200

    Use Async HTTP cluster client

--- examples/cluster/client.rb
+++ examples/cluster/client.rb
@@ -4,26 +4,27 @@
 # Released under the MIT License.
 # Copyright, 2026, by Samuel Williams.
 
-require "net/http"
-require "uri"
+require "async/http/internet/instance"
 
-uri = URI(ENV.fetch("ENVOY_URI", "http://127.0.0.1:10000"))
+url = ENV.fetch("ENVOY_URI", "http://127.0.0.1:10000")
 deadline = Process.clock_gettime(Process::CLOCK_MONOTONIC) + 20
 workers = {}
 
-until workers.size == 2 || Process.clock_gettime(Process::CLOCK_MONOTONIC) >= deadline
-	begin
-		response = Net::HTTP.get_response(uri)
-		
-		if response.is_a?(Net::HTTPSuccess)
-			worker_id = response["x-worker-id"]
-			workers[worker_id] ||= response.body
+Sync do
+	until workers.size == 2 || Process.clock_gettime(Process::CLOCK_MONOTONIC) >= deadline
+		begin
+			Async::HTTP::Internet.get(url) do |response|
+				if response.success?
+					worker_id = response.headers["x-worker-id"]
+					workers[worker_id] ||= response.read
+				end
+			end
+		rescue Errno::ECONNREFUSED, EOFError
+			# Envoy may still be connecting to the xDS control plane.
 		end
-	rescue Errno::ECONNREFUSED, EOFError
-		# Envoy may still be connecting to the xDS control plane.
+		
+		sleep(0.1) unless workers.size == 2
 	end
-	
-	sleep(0.1) unless workers.size == 2
 end
 
 abort "Envoy did not route requests to both workers." unless workers.size == 2
